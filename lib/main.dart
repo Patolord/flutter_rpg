39771d07666fe3f5da29ccea5d04b504018f5721
Patolord <rodrigo_luizp@hotmail.com>
2024-03-16 18:04:06 -0300

    Add CharacterStore and Provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/services/character_store.dart';
 import 'package:flutter_rpg/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MaterialApp(home: const Home(), theme: primaryTheme));
+  runApp(ChangeNotifierProvider(
+      create: (context) => CharacterStore(),
+      child: MaterialApp(home: const Home(), theme: primaryTheme)));
 }
 
 class Sandbox extends StatelessWidget {
